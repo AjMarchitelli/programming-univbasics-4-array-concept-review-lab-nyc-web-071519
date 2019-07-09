4214f911ba065d4f2e2630b4051697af94aa129a
Anthony Joseph Marchitelli Jr <amarchitelli1993@gmail.com>
2019-07-09 20:54:13 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,8 @@
 def find_element_index(array, value_to_find)
    if array.include?(value_to_find)
      puts array[]
-   else return nil 
+   else return nil
+   end
 end
 
 def find_max_value(array)
